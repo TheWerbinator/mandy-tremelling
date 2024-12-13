73ff17914f6b8e90c96401a164cbcac3cd7431b9
Shawn <shawnwerber@suu.edu>
2024-12-13 15:39:50 -0700

    Update to correct logo import

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,8 @@
 import clsx from 'clsx'
+import Image from 'next/image'
 import React from 'react'
+
+import mandy from '../../../public/logo-300x300.webp'
 
 interface Props {
   className?: string
@@ -15,15 +18,15 @@ export const Logo = (props: Props) => {
 
   return (
     /* eslint-disable @next/next/no-img-element */
-    <img
-      alt="Mandy Tremelling Logo"
+    <Image
+      alt="Mandy Tremelling logo - fantasy stylized 'MT' in the center, a fountain pen with a sword hilt next to the 'T' and a dragon on top of it, and swirling fern leaves in gold and blue steel all around. Stars dot the small empty spaces at the top and flowers pop from the corners of the letters."
       width={200}
       height={200}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[12rem] w-full', className)}
-      src='../../../public/logo-300x300.webp'
+      src={mandy}
     />
   )
 }
