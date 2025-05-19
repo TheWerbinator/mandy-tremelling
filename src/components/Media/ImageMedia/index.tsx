8de0e156e18f4c5a659e5a22c871e50cadcec107
Shawn <shawnwerber@suu.edu>
2025-05-19 15:04:05 -0600

    fixed image issues
    source url was malformed in component

--- src/components/Media/ImageMedia/index.tsx
+++ src/components/Media/ImageMedia/index.tsx
@@ -47,7 +47,8 @@ export const ImageMedia: React.FC<MediaProps> = (props) => {
     height = fullHeight!
     alt = altFromResource || ''
 
-    src = `${getClientSideURL()}${url}`
+    src = `${url}`
+    // src = `${getClientSideURL()}${url}`
   }
 
   const loading = loadingFromProps || 'lazy'
